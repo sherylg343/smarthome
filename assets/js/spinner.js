7d963d5d7ab5c438708ad1863767717ceec0a029
sherylg343 <sheryl.s.goldberg@gmail.com>
2020-07-01 02:56:11 +0000

    box styling and initial spinner code

---
+++ assets/js/spinner.js
@@ -0,0 +1,27 @@
+//jquery code from jQuery User Interface (https://jqueryui.com/spinner/)
+$( function() {
+    var spinner = $( "#spinner" ).spinner();
+ 
+    $( "#disable" ).on( "click", function() {
+      if ( spinner.spinner( "option", "disabled" ) ) {
+        spinner.spinner( "enable" );
+      } else {
+        spinner.spinner( "disable" );
+      }
+    });
+    $( "#destroy" ).on( "click", function() {
+      if ( spinner.spinner( "instance" ) ) {
+        spinner.spinner( "destroy" );
+      } else {
+        spinner.spinner();
+      }
+    });
+    $( "#getvalue" ).on( "click", function() {
+      alert( spinner.spinner( "value" ) );
+    });
+    $( "#setvalue" ).on( "click", function() {
+      spinner.spinner( "value", 70 );
+    });
+ 
+    $( "button" ).button();
+  } );
