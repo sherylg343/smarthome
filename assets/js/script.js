e72a794fc39fa6b5fe46fb4d95c3b7eb349410c4
sherylg343 <sheryl.s.goldberg@gmail.com>
2020-07-08 09:20:11 +0000

    first part of scheduler section

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,37 +1,45 @@
 //code from "Add Button Number Incrementers" from *css-tricks, by Chris Coyier, 3/29/13, (https://css-tricks.com/number-increment-buttons/)
-$(".tempbutton").on("click", function() {
+//$(".tempbutton").on("click", function() {
 
-  var $button = $(this);
-  var oldValue = $tempbutton.parent().find("input").val();
+//  var $button = $(this);
+//  var oldValue = $tempbutton.parent().find("input").val();
 
-  if ($tempbutton.text() == "+") {
-	  var newVal = parseFloat(oldValue) + 1;
-	} else {
+//  if ($tempbutton.text() == "+") {
+//	  var newVal = parseFloat(oldValue) + 1;
+//	} else {
    // Don't allow decrementing below zero
-    if (oldValue > 0) {
-      var newVal = parseFloat(oldValue) - 1;
-    } else {
-      newVal = 0;
-    }
-  }
+//    if (oldValue > 0) {
+//      var newVal = parseFloat(oldValue) - 1;
+//    } else {
+//      newVal = 0;
+//    }
+//  }
 
-  $tempbutton.parent().find("input").val(newVal);
+//  $tempbutton.parent().find("input").val(newVal);
 
-});
+//});
 
 
 //progress bar based on only 1
-var slider = document.getElementById("myRange");
-var output = document.getElementById("demo");
-output.innerHTML = slider.value; // Display the default slider value
+//var slider = document.getElementById("myRange");
+//var output = document.getElementById("demo");
+//output.innerHTML = slider.value; // Display the default slider value
 
 // Update the current slider value (each time you drag the slider handle)
-slider.oninput = function() {
-  output.innerHTML = this.value;
-}
+//slider.oninput = function() {
+//  output.innerHTML = this.value;
+//}
 
 //code from solodev: "Adding a DateTime Picker to your Forms", by Matthieu McClintock, (https://www.solodev.com/blog/web-design/adding-a-datetime-picker-to-your-forms.stml)
-  $(function () {
-    $('#datetimepicker1').datetimepicker();
-    $('#datetimepicker2').datetimepicker();
+  $(function() {
+    $('#startdate').datetimepicker({
+        format: 'DD/MM/YYYY',
+        icons: {
+            previous: "fas fa-chevron-left",
+            next: "fas fa-chevron-right",
+        },
+    });
+    $('.input-group-append').click(function(){
+        $('.form-control').trigger('focus');
+    });  
  });
